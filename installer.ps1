24aff3e6af02edcb052645dcd0e5c653db4051f3
Ella <TripingPC@users.noreply.github.com>
2020-06-05 00:09:11 -0400

    add "Vive MV." detection

--- installer.ps1
+++ installer.ps1
@@ -64,6 +64,7 @@ elseif($SteamVRSettingsJson.LastKnown.HMDModel -eq "Oculus Rift S")    {$HMDStat
 elseif($SteamVRSettingsJson.LastKnown.HMDModel -eq "Oculus Quest")     {$HMDStatus = 2}
 elseif($SteamVRSettingsJson.LastKnown.HMDModel -eq "Index")      {$HMDStatus = 3}
 elseif($SteamVRSettingsJson.LastKnown.HMDModel -eq "Vive. MV")         {$HMDStatus = 4}
+elseif($SteamVRSettingsJson.LastKnown.HMDModel -eq "Vive MV.")         {$HMDStatus = 4}
 elseif($SteamVRSettingsJson.LastKnown.HMDModel -eq "VIVE_Pro MV")     {$HMDStatus = 5}
 elseif($SteamVRSettingsJson.LastKnown.HMDModel -eq "HTC Vive Cosmos")  {$HMDStatus = 6}
 elseif($SteamVRSettingsJson.LastKnown.HMDManufacturer -eq "WindowsMR") {$HMDStatus = 7}
